47004e0cbc528b318a9560874195e2b89a667f84
Tyson Gach <tyson@tysongach.com>
2017-02-27 23:04:42 -0500

    wip

--- config.rb
+++ config.rb
@@ -16,8 +16,8 @@ activate :external_pipeline,
 
 proxy "_redirects", "netlify-redirects", ignore: true
 
-page "/advanced/*", layout: "with_sidebar"
-page "/basics/*", layout: "with_sidebar"
+page "/advanced/*", layout: "documentation"
+page "/basics/*", layout: "documentation"
 
 configure :development do
   activate :livereload do |reload|
